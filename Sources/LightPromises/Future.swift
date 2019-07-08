87061655ebf5ee551d2d9fc6b374b51dbcc131a4
Bartek Żabicki <bartekzabicki@gmail.com>
2019-07-08 17:55:10 +0200

    Add final and always

--- Sources/LightPromises/Future.swift
+++ Sources/LightPromises/Future.swift
@@ -10,7 +10,6 @@ import Foundation
 
 public class Future<Value>: Hashable {
   let id = UUID()
-  typealias T = Value
   internal var result: Result<Value, Error>? {
     didSet { result.map(report) }
   }
@@ -63,31 +62,31 @@ extension Future {
 
 extension Future {
   public func flatMap<NextValue>(with closure: @escaping (Value) throws -> Future<NextValue>) -> Future<NextValue> {
-    let promise = Promise<NextValue>()
-    
-    observe { result in
-      switch result {
-      case .success(let value):
-        do {
-          let future = try closure(value)
-          
-          future.observe { result in
-            switch result {
-            case .success(let value):
-              promise.resolve(with: value)
-            case .failure(let error):
-              promise.reject(with: error)
+    return Future<NextValue> { [weak self] promise in
+      
+      guard let self = self else { return }
+      self.observe { result in
+        switch result {
+        case .success(let value):
+          do {
+            let future = try closure(value)
+            
+            future.observe { result in
+              switch result {
+              case .success(let value):
+                promise.resolve(with: value)
+              case .failure(let error):
+                promise.reject(with: error)
+              }
             }
+          } catch {
+            promise.reject(with: error)
           }
-        } catch {
+        case .failure(let error):
           promise.reject(with: error)
         }
-      case .failure(let error):
-        promise.reject(with: error)
       }
     }
-    
-    return promise
   }
   
   public func transformed<NextValue>(with closure: @escaping (Value) throws -> NextValue) -> Future<NextValue> {
@@ -95,6 +94,26 @@ extension Future {
       return try Promise(value: closure(value))
     }
   }
+  
+  public func finally(with closure: @escaping (Value) -> Void) {
+    observe { result in
+      switch result {
+      case .success(let value):
+        closure(value)
+      default: break
+      }
+    }
+  }
+  
+  public func `do`(with closure: @escaping (Value) -> Void) { finally(with: closure) }
+  
+ 
+  public func always(with closure: @escaping () -> Void) {
+    observe { result in
+      closure()
+    }
+  }
+  
 }
 
 extension URLSession {
